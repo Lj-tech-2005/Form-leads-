1990dfcd9322486f253e4f0d4909a1b19709900a
Lj-tech-2005 <lokeshrjj2005@gmail.com>
2025-09-05 16:21:57 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,36 @@
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import leadrouters from "./routers/leadsrouters.js";
-
-dotenv.config();
-
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
 const server = express();
+const cors = require("cors");
+const leadrouters = require('./routers/leadsrouters');
 
 const allowedOrigins = ["http://localhost:5173"];
 
 server.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
 );
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use("/leads", leadrouters);
 
-mongoose
-  .connect(process.env.MONGODB_URL, { dbName: "leads" })
-  .then(() => {
-    console.log("mongodb is connected successfully");
-    server.listen(5000, () => {
-      console.log("server is running on http://localhost:5000");
+mongoose.connect(process.env.MONGODB_URL, { dbName: "leads" })
+    .then(() => {
+        console.log("mongodb is connected successfully");
+        server.listen(5000, () => {
+            console.log("server is running on http://localhost:5000");
+        });
+    })
+    .catch((err) => {
+        console.log("mongodb connection error:", err);
     });
-  })
-  .catch((err) => {
-    console.log("mongodb connection error:", err);
-  });
